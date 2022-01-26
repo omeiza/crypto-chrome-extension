ab6d32cc237a576bb03a97124052d081dbc15e82
Omeiza Owuda <owudaomeiza@gmail.com>
2022-01-26 11:37:41 +0100

    Change to fetch api

--- src/page-override/script.js
+++ src/page-override/script.js
@@ -4,11 +4,9 @@
 const cryptoChromeExtension = {};
 
 cryptoChromeExtension.objectToArray = (thisObject) => {
-    let newArray = Object.keys(thisObject).map((k) => {
+    return Object.keys(thisObject).map((k) => {
         return thisObject[k];
     });
-
-    return newArray;
 }
 
 cryptoChromeExtension.initCrypto = () => {
@@ -17,56 +15,53 @@ cryptoChromeExtension.initCrypto = () => {
     tableRowBody = table.querySelector('.crypto__list--body'),
     cryptoObject = JSON.parse('[{"Bitcoin": "BTC","Ethereum": "ETH","Ripple": "XRP","Bitcoin Cash": "BCH","Cardano": "ADA","Litecoin": "LTC","Dash": "DASH","Monero": "XHR","Bitcoin Gold": "BTG","Ethereum Classic": "ETC","Zcash": "ZEC"}]');
 
-    let cryptoArray = cryptoChromeExtension.objectToArray(cryptoObject[0]);
-    fsyms = new Array();
+    let cryptoArray = cryptoChromeExtension.objectToArray(cryptoObject[0]),
+        fsyms = [];
 
     for (let a = 0; a <= cryptoArray.length - 1; a++) {
         fsyms.push(cryptoArray[a]);
     }
+
     fsyms = fsyms.join(",");
 
-    let request = new XMLHttpRequest();
-    request.open('GET', `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${fsyms}&tsyms=USD`);
-    request.responseType = 'json';
+    // Fetch API
+    fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${fsyms}&tsyms=USD`)
+        .then((response) => {
+            return response.json()
+        })
 
-    request.onload = function() {
-      if (request.status === 200) {
-        let responseArray = cryptoChromeExtension.objectToArray(request.response["DISPLAY"]);
+        .then((data) => {
+            let dataArray = cryptoChromeExtension.objectToArray(data['DISPLAY']);
 
-        if (responseArray.length > 0) {
-            table.style.display = 'table';
-            document.querySelector('.credit').style.display = 'block';
-        }
+            if (dataArray.length > 0) {
+                table.style.display = 'table';
+                document.querySelector('.credit').style.display = 'block';
+            }
 
-        for (let z = 0; z <= responseArray.length - 1; z++) {
-            let clone = tableRowTemplate.cloneNode(true);
-            tableRowBody.appendChild(clone);
+            for (let z = 0; z <= dataArray.length - 1; z++) {
+                let clone = tableRowTemplate.cloneNode(true);
+                tableRowBody.appendChild(clone);
 
-            let x = clone.querySelector('[data-role=numbering]');
-            if (x) x.innerHTML = z + 1;
+                let x = clone.querySelector('[data-role=numbering]');
+                if (x) x.innerHTML = `${z + 1}`;
 
-            x = clone.querySelector('[data-role=symbol]');
-            if (x) x.innerHTML = responseArray[z]["USD"]["FROMSYMBOL"];
+                x = clone.querySelector('[data-role=symbol]');
+                if (x) x.innerHTML = dataArray[z]["USD"]["FROMSYMBOL"];
 
-            x = clone.querySelector('[data-role=price]');
-            if (x) x.innerHTML = responseArray[z]["USD"]["PRICE"];
+                x = clone.querySelector('[data-role=price]');
+                if (x) x.innerHTML = dataArray[z]["USD"]["PRICE"];
 
-            x = clone.querySelector('[data-role=volume]');
-            if (x) x.innerHTML = responseArray[z]["USD"]["SUPPLY"];
+                x = clone.querySelector('[data-role=volume]');
+                if (x) x.innerHTML = dataArray[z]["USD"]["SUPPLY"];
 
-            x = clone.querySelector('[data-role=change]');
-            if (x) x.innerHTML = responseArray[z]["USD"]["CHANGE24HOUR"];
-        }
-      } else {
-        console.log('Error loading crypto data:' + request.statusText);
-      }
-    };
+                x = clone.querySelector('[data-role=change]');
+                if (x) x.innerHTML = dataArray[z]["USD"]["CHANGE24HOUR"];
+            }
+        })
 
-    request.onerror = function() {
-        console.log('There was a network error');
-    };
-
-    request.send();
+        .catch((err) => {
+            console.error('Error loading crypto data:' + err);
+        })
 }
 
 cryptoChromeExtension.initCrypto();
